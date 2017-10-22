1bd9df01081d3ba71f7dd03dcbb79705a162a9b9
Porridge221 <ivanov.u97@gmail.com>
2017-10-23 07:15:30 +1000

    add new tests
    all tests pass

--- test/Parser/MathParserTest.java
+++ test/Parser/MathParserTest.java
@@ -18,7 +18,15 @@ public class MathParserTest {
     @Test
     public void getSignal() throws Exception {
         parser = new MathParser("2");
-        assertEquals(2, parser.Execute(), epsilon);
+        assertEquals(MathParser.Signals.ID, parser.getSignal("sina"));
+    }
+
+    @Test
+    public void setVariable() throws Exception {
+        parser = new MathParser("a + 2");
+        parser.setVariable("a", 163.123);
+        parser.setVariable("abc", 15.3);
+        assertEquals(163.123, parser.getVariable("a"), epsilon);
 
     }
 
@@ -126,6 +134,13 @@ public class MathParserTest {
     public void testExecute_17() throws Exception {
         parser = new MathParser("2-(-2)");
         assertEquals(2-(-2), parser.Execute(), epsilon);
+    }
+
+    @Test
+    public void testExecute_18() throws Exception {
+        parser = new MathParser("sin(-(-2-2)*sina)-2");
+        parser.setVariable("sina", 15.5);
+        assertEquals(Math.sin(-(-2-2)*15.5)-2, parser.Execute(), epsilon);
     }
 
 }
